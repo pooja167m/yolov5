59550905d541a414ce23e85d09eebb6136596547
pooja167m <pooja167m@gmail.com>
2025-03-31 15:55:01 +0530

    Update common.py
    reakyrelu
    
    Signed-off-by: pooja167m <pooja167m@gmail.com>

--- models/common.py
+++ models/common.py
@@ -73,7 +73,8 @@ def autopad(k, p=None, d=1):
 class Conv(nn.Module):
     """Applies a convolution, batch normalization, and activation function to an input tensor in a neural network."""
 
-    default_act = nn.SiLU()  # default activation
+    default_act = nn.LeakyReLU(negative_slope=0.01)  # Change activation function
+
 
     def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
         """Initializes a standard convolution layer with optional batch normalization and activation."""
@@ -81,6 +82,7 @@ class Conv(nn.Module):
         self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
         self.bn = nn.BatchNorm2d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
 
     def forward(self, x):
         """Applies a convolution followed by batch normalization and an activation function to the input tensor `x`."""
